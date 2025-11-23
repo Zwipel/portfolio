fa53ddb5ced893434cdafb850ccfad66636e4174
DennisButenko <buden1998@gmail.com>
2025-11-23 14:11:44 +0100

    changed name

--- portfolio/.next/types/app/contact/page.ts
+++ portfolio/.next/types/app/contact/page.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\Zwopi\Development\application\my-portfolio\app\projects\page.tsx
-import * as entry from '../../../../app/projects/page.js'
+// File: C:\Users\Zwopi\Development\application\my-portfolio\app\contact\page.tsx
+import * as entry from '../../../../app/contact/page.jsx'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../app/projects/page.js')
+type TEntry = typeof import('../../../../app/contact/page.jsx')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
